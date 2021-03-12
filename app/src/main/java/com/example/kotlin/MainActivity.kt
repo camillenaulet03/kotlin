acc694810caec35ccf6d4e8f9626102e2a814cb1
Johan <johdel390@gmail.com>
2021-03-12 16:17:02 +0100

    RecyclerView

--- app/src/main/java/com/example/kotlin/MainActivity.kt
+++ app/src/main/java/com/example/kotlin/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.kotlin.helpers.AppDatabaseHelper
+import com.example.kotlin.adapters.CarAdapter
 import com.example.kotlin.models.Car
 
 class MainActivity : AppCompatActivity() {
@@ -10,11 +13,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val carsList = AppDatabaseHelper.getDatabase(this).countriesDAO().getCarList()
+
+        // Initialize RecyclerView
+        val recycler = findViewById<RecyclerView>(R.id.cars_list)
+        recycler.setHasFixedSize(true)
+
+        val layoutManager = LinearLayoutManager(this)
+        recycler.layoutManager = layoutManager
+
+        val adapter = CarAdapter(carsList)
+        recycler.adapter = adapter
+
+        //
         val car = Car(0,"Opel Corsa",2.0,"A","fromage.png")
         AppDatabaseHelper.getDatabase(this).countriesDAO().insert(car)
 
         println("---- LES VOITURES DU GARAGE ----")
-        println(AppDatabaseHelper.getDatabase(this).countriesDAO().getListeCountries().toString())
+        println(AppDatabaseHelper.getDatabase(this).countriesDAO().getCarList().toString())
 
     }
 }
---
+++ app/src/main/java/com/example/kotlin/adapters/CarAdapter.kt
@@ -0,0 +1,48 @@
+package com.example.kotlin.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.kotlin.R
+import com.example.kotlin.models.Car
+import com.squareup.picasso.Picasso
+
+class CarAdapter(private var carList: MutableList<Car>) : RecyclerView.Adapter<CarAdapter.CarViewHolder>()
+{
+    var onItemClick: ((Car) -> Unit)? = null
+    // Crée chaque vue item à afficher :
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CarViewHolder      {
+        val viewCourse = LayoutInflater.from(parent.context).inflate(R.layout.item_list, parent, false)
+        return CarViewHolder(viewCourse)
+    }
+// Renseigne le contenu de chaque vue item :
+    override fun onBindViewHolder(holder: CarViewHolder, position: Int)      {
+
+    holder.textViewCarName .text = carList[position].name
+    holder.textViewCarPrice.text = carList[position].price.toString()
+    holder.textViewCarCO2.text = carList[position].co2
+
+    Picasso.get().load("http://s519716619.onlinehome.fr/exchange/madrental/images/"+carList[position].image).fit().centerInside().into(holder.imageViewCarPicture)
+    }
+
+    override fun getItemCount(): Int = carList.size
+
+// ViewHolder :
+    inner class CarViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)      {
+        val textViewCarName: TextView = itemView.findViewById(R.id.name)
+        val textViewCarPrice: TextView = itemView.findViewById(R.id.price)
+        val textViewCarCO2: TextView = itemView.findViewById(R.id.category)
+        val imageViewCarPicture: ImageView = itemView.findViewById(R.id.image)
+
+        init {
+            itemView.setOnClickListener {
+                onItemClick?.invoke(carList[adapterPosition])
+            }
+        }
+    }
+
+
+}
